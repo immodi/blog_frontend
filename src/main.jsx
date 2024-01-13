613b26325f0f51b4b1c768dd9021e451fc053149
immodi <ay727467@gmail.com>
2024-01-13 14:12:08 +0200

    adasdad

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './style/index.css'
 import App from './App'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        <App />
     </React.StrictMode>,
 )
