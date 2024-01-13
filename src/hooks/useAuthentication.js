0c2662a6ed9ab111de6a0bc732620dd4f141d92f
immodi <ay727467@gmail.com>
2024-01-13 12:27:57 +0200

    ss

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -3,19 +3,27 @@ import axios from "axios";
 
 let URL;
 
-function useAuthentication(url, setMessagesObject, userName=null, password=null) {
+export function useAuthentication(url, setMessagesObject, cookies, setCookie, removeCookie, userName=null, password=null) {
     URL = url
-    const [authenticated, setAuthenticated] = useState(false)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [isAdmin, setIsAdmin] = useState(false)
     const [token, setToken] = useState(null)
     const [messageOptions, setMessageOptions] = useState(null)
     const [credentials, setCredentials] = useState({
-        userName: userName,
+        userName: cookies['token-cookie'] ? cookies['token-cookie'].userName : userName,
         password: password
     })
 
     useEffect(() => {
-        requestToken(credentials, setAuthenticated, token, setToken, setMessageOptions, setIsAdmin)
+        if (cookies['token-cookie']) {
+            authenticateWithToken(cookies['token-cookie'].token, setIsAuthenticated, setToken, setIsAdmin, setMessageOptions, setCredentials)
+        }
+    }, [])
+
+    useEffect(() => {
+        if ((credentials.password != null && credentials.userName != null) || (credentials.password == null && credentials.userName == null)) {
+            requestToken(credentials, setIsAuthenticated, token, setToken, setMessageOptions, setIsAdmin, cookies, setCookie, removeCookie)
+        }
     }, [credentials])
 
     useEffect(() => {
@@ -24,10 +32,10 @@ function useAuthentication(url, setMessagesObject, userName=null, password=null)
         }
     }, [messageOptions])
 
-    return [authenticated, isAdmin, token, credentials, setCredentials]
+    return [isAuthenticated, isAdmin, token, credentials, setCredentials]
 }
 
-function requestToken(credentials, setAuthenticated, token, setToken, setMessageOptions, setIsAdmin) {
+function requestToken(credentials, setIsAuthenticated, token, setToken, setMessageOptions, setIsAdmin, cookies, setCookie, removeCookie) {
     if (credentials.userName != null) {
         let userName = credentials.userName
         let password = credentials.password
@@ -36,19 +44,25 @@ function requestToken(credentials, setAuthenticated, token, setToken, setMessage
             password: password
         }).then(res => {
             // login successful
-            setAuthenticated(true)
+            setCookie('token-cookie', {
+                userName: userName,
+                token: res.data.token,
+            })
+            setIsAuthenticated(true)
             setToken(res.data.token)
-            isAdmin(userName, setIsAdmin, setMessageOptions)
-        }).catch((e) => {
+            isAdminFunc(userName, setIsAdmin, setMessageOptions)
+        }).catch(e => {
             // login failed
-            setAuthenticated(false)
+            setCookie('token-cookie', null, {maxAge: 30*60*60})
+            setIsAuthenticated(false)
             setToken(null)
             sendMessage(setMessageOptions, 'error', e.message)
         })
     } else {
         if (token != null) {
-            // looging out
-            setAuthenticated(false)
+            // logging out
+            removeCookie('token-cookie')
+            setIsAuthenticated(false)
             setToken(null)
             setIsAdmin(false)
             sendMessage(setMessageOptions, 'success', 'Logged Out :/')
@@ -56,7 +70,7 @@ function requestToken(credentials, setAuthenticated, token, setToken, setMessage
     }
 }
 
-async function isAdmin(userName, setIsAdmin, setMessageOptions) {
+export async function isAdminFunc(userName, setIsAdmin, setMessageOptions) {
     try {
         let res = await axios.post(`${URL}user`, {username: userName}, {
             headers: {
@@ -77,4 +91,24 @@ function sendMessage(setMessageOptions, status, message) {
         message: message
     })
 }
-export default useAuthentication
+
+export async function authenticateWithToken(token, setIsAuthenticated, setToken, setIsAdmin, setMessageOptions, setCredentials) {
+    try {
+        let res = await axios.post(`${URL}user/get-with-token`, {token: token}, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+    
+        setIsAuthenticated(true)
+        setToken(token)
+        isAdminFunc(res.data.username, setIsAdmin, setMessageOptions)
+        setCredentials({
+            userName: res.data.username,
+            password: null
+        })      
+
+    } catch (err) {
+        console.log(err.message);
+    }
+}
