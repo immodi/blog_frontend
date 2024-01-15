771c5a540cb06a703e69384680fd8d01d5de4086
immodi <ay727467@gmail.com>
2024-01-15 12:35:17 +0200

    ad

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.join(__dirname)
+  base: '',
+  root: path.join(__dirname),
 })
