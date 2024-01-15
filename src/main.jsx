393231a61926c6a339b5fdfcc5a570628a623e6f
immodi <ay727467@gmail.com>
2024-01-15 12:26:22 +0200

    APP_PATH

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style/index.css'
-import App from './App.jsx'
+import App from '/src/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
 )
+
+// /vercel/path0
